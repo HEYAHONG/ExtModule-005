b3c145bf5c1090b950f57075a23a3d79965b10a1
HEYAHONG <2229388563@qq.com>
2025-12-31 15:16:25 +0800

    启用hbox节（section）支持

--- FW/001-base/main/hbox_config.h
+++ FW/001-base/main/hbox_config.h
@@ -6,4 +6,22 @@
 #define HDEFAULTS_MUTEX_UNLOCK hbox_exit_critical
 
 
+
+
+/*
+ * 启用初始化节
+ */
+#define HRUNTIME_USING_INIT_SECTION 1
+
+/*
+ * 启用循环节
+ */
+#define HRUNTIME_USING_LOOP_SECTION 1
+
+/*
+ * 启用符号节
+ */
+#define HRUNTIME_USING_SYMBOL_SECTION 1
+
+
 #endif  // __HBOX_CONFIG_H__
--- FW/001-base/main/main.c
+++ FW/001-base/main/main.c
@@ -9,11 +9,30 @@ static const char *TAG = "esp32 main";
 
 void app_main(void)
 {
-	ESP_LOGI(TAG,"app main enter!");
-	hcpprt_init();
-	while(true)
-	{
-		hcpprt_loop();
-		vTaskDelay(1);
-	}
+    hcpprt_init();
+    while(true)
+    {
+        hcpprt_loop();
+        vTaskDelay(1);
+    }
 }
+
+/*
+ * 主初始化
+ */
+void  main_init(const hruntime_function_t *func)
+{
+    ESP_LOGI(TAG,"app main enter!");
+}
+HRUNTIME_INIT_EXPORT(main,255,main_init,NULL);
+HRUNTIME_SYMBOL_EXPORT(main_init);
+
+/*
+ * 主循环
+ */
+void  main_loop(const hruntime_function_t *func)
+{
+
+}
+HRUNTIME_LOOP_EXPORT(main,255,main_loop,NULL);
+HRUNTIME_SYMBOL_EXPORT(main_loop);
