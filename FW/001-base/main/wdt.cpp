0f7d7b6216d11f7bbc39e7aa16e6da4f30286565
HEYAHONG <2229388563@qq.com>
2025-12-31 16:50:58 +0800

    [001-base] 移植软件看门狗

---
+++ FW/001-base/main/wdt.cpp
@@ -0,0 +1,42 @@
+#include "hbox_config.h"
+#include "hbox.h"
+#include "esp_log.h"
+#include "esp_system.h"
+static const char *TAG = "watchdog";
+
+static void sys_reset()
+{
+    esp_restart();
+    //若复位未实现，使用死循环通过硬件看门狗复位
+    while(true)
+    {
+
+    }
+}
+
+static void hw_feed()
+{
+
+}
+
+/*
+ * 初始化
+ */
+void  watchdog_init(const hruntime_function_t *func)
+{
+    ESP_LOGI(TAG,"init!");
+	//设置好看门狗
+    hwatchdog_set_hardware_dog_feed(hw_feed);
+    hwatchdog_setup_software_dog(sys_reset,hdefaults_tick_get);
+}
+HRUNTIME_INIT_EXPORT(watchdog,0,watchdog_init,NULL);
+
+/*
+ * 循环
+ */
+void  watchdog_loop(const hruntime_function_t *func)
+{
+
+}
+HRUNTIME_LOOP_EXPORT(watchdog,0,watchdog_loop,NULL);
+
