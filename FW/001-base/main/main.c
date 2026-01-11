409f518b3f21c3988912aa8c4ada5564d3344bad
HEYAHONG <2229388563@qq.com>
2026-01-11 16:48:22 +0800

    [001-base] 完善nvs初始化

--- FW/001-base/main/main.c
+++ FW/001-base/main/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include "nvs_flash.h"
 #include "esp_system.h"
 #include "esp_log.h"
 #include "esp_task.h"
@@ -74,6 +75,16 @@ void app_main(void)
      * 对于newlib而言，时区可通过environ环境变量设置，调用tzset生效
      */
     tzset();
+
+    {
+        //初始化nvs_flash
+        esp_err_t ret = nvs_flash_init();
+        if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+        {
+            nvs_flash_erase();
+            nvs_flash_init();
+        }
+    }
 
     /*
      * 启动hruntime
