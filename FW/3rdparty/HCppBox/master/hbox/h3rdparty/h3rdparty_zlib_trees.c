9c92d6a44d650dc554194dd928b3aefacd546c18
HEYAHONG <2229388563@qq.com>
2026-03-11 09:21:16 +0800

    [HCppBox] 更新HCppBox

--- FW/3rdparty/HCppBox/master/hbox/h3rdparty/h3rdparty_zlib_trees.c
+++ FW/3rdparty/HCppBox/master/hbox/h3rdparty/h3rdparty_zlib_trees.c
@@ -45,7 +45,24 @@
 #undef open
 #endif
 #define open hopen
+#ifdef fcntl
+#undef fcntl
+#endif
+#define fcntl hfcntl
 
+#else
+
+#undef O_NONBLOCK
+#undef O_CLOEXEC
+
+#endif
+
+#ifndef EAGAIN
+#define EAGAIN HEAGAIN
+#endif
+
+#ifndef EWOULDBLOCK
+#define EWOULDBLOCK HEAGAIN
 #endif
 
 #include "stdarg.h"
