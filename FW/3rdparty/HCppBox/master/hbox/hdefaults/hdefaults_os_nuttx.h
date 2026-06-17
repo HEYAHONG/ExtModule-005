0bfbc47b5bb5e6739658d646aec09313e05b288c
HEYAHONG <2229388563@qq.com>
2026-06-17 15:31:50 +0800

    [FW/3rdparty/HCppBox] 更新HCppBox

--- FW/3rdparty/HCppBox/master/hbox/hdefaults/hdefaults_os_nuttx.h
+++ FW/3rdparty/HCppBox/master/hbox/hdefaults/hdefaults_os_nuttx.h
@@ -11,6 +11,40 @@
 #include "hdefaults_common.h"
 
 
+#if !defined(HAVE_UNISTD_H)
+#define HAVE_UNISTD_H  1
+#include "unistd.h"
+#include "pthread.h"
+#endif
+
+#if !defined(HAVE_FCNTL_H)
+#define HAVE_FCNTL_H   1
+#define HAVE_NO_OPENAT 1
+#endif
+
+/*
+ * NuttX采用pthread管理线程
+ */
+#if !defined(HTHRD_USING_PTHREAD)
+#define HTHRD_USING_PTHREAD 1
+#endif
+
+/*
+ * NuttX采用pthread管理锁
+ */
+#if !defined(HMTX_USING_PTHREAD)
+#define HMTX_USING_PTHREAD 1
+#endif
+
+
+/*
+ * NuttX支持具有完善的C++支持
+ */
+#ifndef HCPPRT_NO_NEW_AND_DELETE_OVERRIDE
+#define HCPPRT_NO_NEW_AND_DELETE_OVERRIDE 1
+#endif // HCPPRT_NO_NEW_AND_DELETE_OVERRIDE
+
+
 /*
  * 系统调用号
  */
